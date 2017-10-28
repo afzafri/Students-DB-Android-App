c5f7af6ed85d3521817d9dc121101c30243a27a5
afzafri <zafri_21596@yahoo.com>
2017-10-28 17:53:51 +0800

    initialize input fields

--- app/src/main/java/com/afifzafri/studentsdb/InsertData.java
+++ app/src/main/java/com/afifzafri/studentsdb/InsertData.java
@@ -2,6 +2,7 @@ package com.afifzafri.studentsdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class InsertData extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class InsertData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_data);
+
+        // initialize input fields
+        EditText studID = (EditText)findViewById(R.id.editStudID);
+        EditText name = (EditText)findViewById(R.id.editName);
+        EditText ic = (EditText)findViewById(R.id.editIC);
+        EditText dob = (EditText)findViewById(R.id.editDOB);
+        EditText address = (EditText)findViewById(R.id.editAddress);
+        EditText program = (EditText)findViewById(R.id.editProgram);
+        EditText phone = (EditText)findViewById(R.id.editPhone);
+        EditText email = (EditText)findViewById(R.id.editEmail);
+
+
     }
 }
