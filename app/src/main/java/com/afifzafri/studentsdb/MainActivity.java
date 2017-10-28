fc1175711b4e2149df1d7e1697237ad07394e0ef
afzafri <zafri_21596@yahoo.com>
2017-10-28 16:29:13 +0800

    initialize buttons and set on click listener

--- app/src/main/java/com/afifzafri/studentsdb/MainActivity.java
+++ app/src/main/java/com/afifzafri/studentsdb/MainActivity.java
@@ -2,6 +2,8 @@ package com.afifzafri.studentsdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the buttons
+        Button btnInsertDataPage = (Button)findViewById(R.id.btnSearchDataPage);
+        Button btnViewDataPage = (Button)findViewById(R.id.btnViewDataPage);
+        Button btnSearchDataPage = (Button)findViewById(R.id.btnSearchDataPage);
+
+        // open insert data activity page
+        btnInsertDataPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // action here
+            }
+        });
+
+        // open view data activity page
+        btnViewDataPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // action here
+            }
+        });
+
+        // open search data activity page
+        btnSearchDataPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // action here
+            }
+        });
     }
 }
