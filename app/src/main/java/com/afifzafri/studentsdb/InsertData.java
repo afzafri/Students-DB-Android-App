07c574d5db5fd27270bfea0daa4317a9309d67d0
afzafri <zafri_21596@yahoo.com>
2017-10-28 17:57:59 +0800

    set on click listener for save and clear button

--- app/src/main/java/com/afifzafri/studentsdb/InsertData.java
+++ app/src/main/java/com/afifzafri/studentsdb/InsertData.java
@@ -2,6 +2,8 @@ package com.afifzafri.studentsdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class InsertData extends AppCompatActivity {
@@ -11,7 +13,7 @@ public class InsertData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_data);
 
-        // initialize input fields
+        // initialize input fields and buttons
         EditText studID = (EditText)findViewById(R.id.editStudID);
         EditText name = (EditText)findViewById(R.id.editName);
         EditText ic = (EditText)findViewById(R.id.editIC);
@@ -20,6 +22,23 @@ public class InsertData extends AppCompatActivity {
         EditText program = (EditText)findViewById(R.id.editProgram);
         EditText phone = (EditText)findViewById(R.id.editPhone);
         EditText email = (EditText)findViewById(R.id.editEmail);
+        Button btnClear = (Button)findViewById(R.id.btnClear);
+        Button btnSave = (Button)findViewById(R.id.btnSave);
+
+        // button on click action
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // action here
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // action here
+            }
+        });
 
 
     }
