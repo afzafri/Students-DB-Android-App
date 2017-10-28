a92303e9c0e9b5258d397b437742e3695f050434
afzafri <zafri_21596@yahoo.com>
2017-10-28 17:30:47 +0800

    created new activity for Edit and View data page

---
+++ app/src/main/java/com/afifzafri/studentsdb/EditViewData.java
@@ -0,0 +1,13 @@
+package com.afifzafri.studentsdb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditViewData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_view_data);
+    }
+}
