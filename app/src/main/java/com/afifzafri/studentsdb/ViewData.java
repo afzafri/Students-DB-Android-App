356fc3a0294e662dbe04effc9eb09bb314ea8565
afzafri <zafri_21596@yahoo.com>
2017-10-29 21:30:00 +0800

    test listView

--- app/src/main/java/com/afifzafri/studentsdb/ViewData.java
+++ app/src/main/java/com/afifzafri/studentsdb/ViewData.java
@@ -2,6 +2,8 @@ package com.afifzafri.studentsdb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ViewData extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class ViewData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_data);
+
+        String[] listdata = {"afif","syafiq","acab","yusri"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listdata);
+
+        ListView listView = (ListView) findViewById(R.id.listData);
+        listView.setAdapter(adapter);
     }
 }
