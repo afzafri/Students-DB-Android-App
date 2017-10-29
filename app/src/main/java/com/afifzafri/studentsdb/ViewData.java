e0243fa4de95e2cc9316966b261d4a052fed6965
afzafri <zafri_21596@yahoo.com>
2017-10-30 01:30:46 +0800

    get value from clicked ListView and test display

--- app/src/main/java/com/afifzafri/studentsdb/ViewData.java
+++ app/src/main/java/com/afifzafri/studentsdb/ViewData.java
@@ -4,9 +4,13 @@ import android.app.ListActivity;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ViewData extends AppCompatActivity {
 
@@ -28,5 +32,15 @@ public class ViewData extends AppCompatActivity {
         ListDataCursorAdapter listdataAdapter = new ListDataCursorAdapter(this, cursor);
         // Attach cursor adapter to the ListView
         listData.setAdapter(listdataAdapter);
+
+        // on click listener for when the list is clicked
+        listData.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView sid = (TextView)view.findViewById(R.id.display_studid); //the 'view' refers to current position of the item
+                String studID = sid.getText().toString();
+                Toast.makeText(getApplicationContext(), studID,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
