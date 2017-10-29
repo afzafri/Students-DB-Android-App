cdd011df96a7e513d51e9dc36007c5e53fbaa54d
afzafri <zafri_21596@yahoo.com>
2017-10-29 11:30:57 +0800

    onclick search button, call search data function

--- app/src/main/java/com/afifzafri/studentsdb/SearchData.java
+++ app/src/main/java/com/afifzafri/studentsdb/SearchData.java
@@ -1,13 +1,33 @@
 package com.afifzafri.studentsdb;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SearchData extends AppCompatActivity {
+
+    // initialize object
+    DBHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_data);
+
+        mydb = new DBHelper(this);
+
+        EditText sid = (EditText)findViewById(R.id.editSearch);
+        Button btnSearch = (Button)findViewById(R.id.btnSearch);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String studid = sid.getText().toString();
+                Cursor cursor = mydb.searchData(studid);
+            }
+        });
     }
 }
