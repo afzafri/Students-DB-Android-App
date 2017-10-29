2cc29df79a6d1be4a8c34a20265aa91707b9e795
afzafri <zafri_21596@yahoo.com>
2017-10-29 22:47:59 +0800

    list data to listView from database

--- app/src/main/java/com/afifzafri/studentsdb/ViewData.java
+++ app/src/main/java/com/afifzafri/studentsdb/ViewData.java
@@ -1,23 +1,32 @@
 package com.afifzafri.studentsdb;
 
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class ViewData extends AppCompatActivity {
+public class ViewData extends ListActivity {
+
+    DBHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_data);
 
-        String[] listdata = {"afif","syafiq","acab","yusri"};
+        // initialize object
+        mydb = new DBHelper(this);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, listdata);
+        Cursor cursor = mydb.listAllData(); // get data from db
 
-        ListView listView = (ListView) findViewById(R.id.listData);
-        listView.setAdapter(adapter);
+        String[] fromColumns = {DBHelper.COLUMN_NAME, DBHelper.COLUMN_ID};
+        int[] toViews = {R.id.display_name, R.id.display_studid};
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.studid_name_layout, cursor, fromColumns, toViews, 0);
+        ListView listdata = getListView();
+        listdata.setAdapter(adapter);
     }
 }
