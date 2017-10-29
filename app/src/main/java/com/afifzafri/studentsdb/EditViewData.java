b5c5fc796e02dd0d96ec7074d0d281dc812b1ad6
afzafri <zafri_21596@yahoo.com>
2017-10-29 12:50:13 +0800

    test get data passed from search activity

--- app/src/main/java/com/afifzafri/studentsdb/EditViewData.java
+++ app/src/main/java/com/afifzafri/studentsdb/EditViewData.java
@@ -1,7 +1,9 @@
 package com.afifzafri.studentsdb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditViewData extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class EditViewData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_view_data);
+
+        Intent intentPage = getIntent();
+        String studID = intentPage.getStringExtra("STUD_ID");
+
+        EditText editStudID = (EditText)findViewById(R.id.editStudID);
+        editStudID.setText(studID);
     }
 }
