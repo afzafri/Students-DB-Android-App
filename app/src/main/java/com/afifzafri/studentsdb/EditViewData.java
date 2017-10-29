c4121a7acb7e4de3d96a5d74c08fee02d0c60660
afzafri <zafri_21596@yahoo.com>
2017-10-29 12:58:58 +0800

    initialize all input field

--- app/src/main/java/com/afifzafri/studentsdb/EditViewData.java
+++ app/src/main/java/com/afifzafri/studentsdb/EditViewData.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditViewData extends AppCompatActivity {
 
@@ -15,8 +16,15 @@ public class EditViewData extends AppCompatActivity {
         Intent intentPage = getIntent();
         String studID = intentPage.getStringExtra("STUD_ID");
 
-        EditText editStudID = (EditText)findViewById(R.id.editStudID);
+        final EditText editStudID = (EditText)findViewById(R.id.editStudID);
         editStudID.setFocusable(false); // disable input
+        final EditText editName = (EditText)findViewById(R.id.editName);
+        final EditText editIC = (EditText)findViewById(R.id.editIC);
+        final EditText editDOB = (EditText)findViewById(R.id.editDOB);
+        final EditText editAddress = (EditText)findViewById(R.id.editAddress);
+        final EditText editProgram = (EditText)findViewById(R.id.editProgram);
+        final EditText editPhone = (EditText)findViewById(R.id.editPhone);
+        final EditText editEmail = (EditText)findViewById(R.id.editEmail);
 
         editStudID.setText(studID);
     }
